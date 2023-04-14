011c5d9f3f202f8a3cf286dd4797a0b5bc2590f5
David Spivey <davidspivey3.1415@gmail.com>
2023-04-14 19:53:34 -0400

    Add basic game loop and ability to exit, window now pops up.

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 import pygame as pg
+import sys
+from pygame.locals import QUIT
+pg.init()
+
+WINDOWSIZE = (400,400)
+DISPLAYSURF = pg.display.set_mode(WINDOWSIZE)
+FPS = pg.time.Clock()
+FRAMERATE = 60
+BLACK = pg.Color(0, 0, 0)         
+WHITE = pg.Color(255, 255, 255)   
+GREY = pg.Color(128, 128, 128)   
+RED = pg.Color(255, 0, 0)       
+
+# Begin main game loop
+while (True):
+  # code codity code hot sausage and mustard
+  for event in pg.event.get():
+    if event.type == QUIT:
+      sys.exit()
+  pg.display.update()
+  FPS.tick(FRAMERATE)
