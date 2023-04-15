fd6973d9be2f717ea9de9ec2cfd2963647c53e20
David Spivey <davidspivey3.1415@gmail.com>
2023-04-15 16:56:16 -0400

    Pretty up main.py, remove extraneous space/comments.

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import pygame as pg
 import sys
 import random
+from pygame.locals import *
+
 from Platform import Platform
 from coin import Coin 
-
-from pygame.locals import *
 from config import * # gets all setting constants
 from player import Player
 
 pg.init()
-vec = pg.math.Vector2 # used for 2 dimensional movement calculations    
-
 background = pg.image.load("PyBackground.jpg")
+FPS = pg.time.Clock()
 
 C1 = Coin(300,200)
-
-FPS = pg.time.Clock()
 DISPLAYSURF = pg.display.set_mode((WIDTH, HEIGHT))
-
 P1 = Player(K_LEFT, K_RIGHT, "BlueRobot.png", (50, 50))
 P2 = Player(K_a, K_d,"RedRobot.png", (WIDTH - 50, 50))
 
@@ -38,23 +34,15 @@ for i in range(random.randint(25,30)): #creates between 18 and 25
       platforms.append(new_platform)
       break
 
-""" moving_plat = []
-for x in range(random.randint(2,6)):
-  j = random.randint(0,len(platforms)-1)
-  moving_plat.append(platforms[j]) """
-
 moving_platforms = random.sample(platforms, random.randint(2,6))
 # Assign a non-zero velocity to each of the selected platforms
 for platform in moving_platforms:
   platform.vel = x_speed
   
-
 all_platforms = pg.sprite.Group()
-#all_platforms.add(moving_plat)
 all_platforms.add(platforms)
 
 game_state = "start"
-
 font = pg.font.SysFont('Arial', 40)
 button = font.render('Begin (SPACE)', True, WHITE)
 buttonrect = button.get_rect()
@@ -65,6 +53,7 @@ bluerect.center = (WIDTH/2, 400)
 red = font.render('Red uses WASD to move', True, RED)
 redrect = red.get_rect()
 redrect.center = (WIDTH/2, 600)
+
 # Begin main game loop
 while (True):
   # check events first
@@ -79,7 +68,6 @@ while (True):
       if event.key == K_w:
         P2.jump(platforms)
     
-  
   if game_state == "start":
     DISPLAYSURF.blit(button, buttonrect)
     DISPLAYSURF.blit(blue, bluerect)
@@ -87,9 +75,7 @@ while (True):
     pg.display.update()
 
   else:
-     # update the players
-
-    if(C1.rect.x < 0 or C1.rect.x > WIDTH or C1.rect.y < 0 or C1.rect.y > HEIGHT):
+    if (C1.rect.x < 0 or C1.rect.x > WIDTH or C1.rect.y < 0 or C1.rect.y > HEIGHT):
       x = random.randint(20,WIDTH-20)
       y = random.randint(20,HEIGHT-20)
 
@@ -107,12 +93,9 @@ while (True):
               C1.rect.y = -1000
               P2.score += 1
 
-    
-    # update the player
     P1.update(all_platforms)
     P2.update(all_platforms)
     
-    # blank out the display surface in preparation for drawing new frame
     DISPLAYSURF.fill(GREY)
     DISPLAYSURF.blit(background,[0,0])
 
@@ -128,9 +111,7 @@ while (True):
         
     DISPLAYSURF.blit(P1.image,P1.rect)
     DISPLAYSURF.blit(P2.image, P2.rect)
-
     DISPLAYSURF.blit(C1.image,C1.rect)
-
 
     pg.display.update()
     FPS.tick(FRAMERATE)
