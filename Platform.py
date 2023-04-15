3ed23f12aa324725c971c93da2b2161d5b1ce911
Wes Deal <wesdeall@gmail.com>
2023-04-15 13:26:24 -0400

    platform generation + possible movement

--- Platform.py
+++ Platform.py
@@ -1,14 +1,18 @@
 import pygame as pg
+import random
+from config import *
+
 
 
 class Platform(pg.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.surf = pg.Surface((50, 10))
-        self.surf.fill(255,255,255)
-        self.rect = self.surf.get_rect()
+        self.surf = pg.Surface((random.randint(70, 130), 10))
+        self.surf.fill(BLACK)
+        self.rect = self.surf.get_rect(topleft=(x, y))
         self.rect.x = x
         self.rect.y = y
+        self.vel = x_speed
 
 
 
