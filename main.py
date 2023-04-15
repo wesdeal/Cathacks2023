f891ff80fee95c6304b2c11f0e31fdaa1da6aeb5
Wes Deal <wesdeall@gmail.com>
2023-04-14 21:35:29 -0400

    added platform class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import sys
+from Platform import Platform
 from pygame.locals import QUIT
 pg.init()
 
@@ -12,11 +13,15 @@ WHITE = pg.Color(255, 255, 255)
 GREY = pg.Color(128, 128, 128)   
 RED = pg.Color(255, 0, 0)       
 
+platform = Platform(x=100, y=200)
+
 # Begin main game loop
 while (True):
   # code codity code hot sausage and mustard
   for event in pg.event.get():
     if event.type == QUIT:
       sys.exit()
+
+  DISPLAYSURF.blit(platform.surf, platform.rect)
   pg.display.update()
   FPS.tick(FRAMERATE)
