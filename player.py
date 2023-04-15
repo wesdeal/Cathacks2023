ee4c64be90d47a6eb0c0ba087a4dafa90cb678e5
David Spivey <davidspivey3.1415@gmail.com>
2023-04-14 23:55:48 -0400

    Fix player movement, change Player class to use config stuff.

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import sys
+from config import *
 from pygame.locals import *
 
 vec = pg.math.Vector2
@@ -8,30 +9,31 @@ class Player(pg.sprite.Sprite):
     def __init__(self):
         super().__init__() 
         self.surf = pg.Surface((30, 30))
-        self.surf.fill((255,0,0))
+        self.surf.fill(RED)
         self.rect = self.surf.get_rect()
 
         self.pos = vec((10, 385))
         self.vel = vec(0,0)
         self.acc = vec(0,0)
+        
     def move(self):
         self.acc = vec(0,0)
         
-        self.acc.x += self.vel.x * -0.12
-        self.vel += self.acc
-        self.pos += self.vel + 0.5 * self.acc
-
-    def update(self):
         pressed_keys = pg.key.get_pressed()
-                
         if pressed_keys[K_LEFT]:
             self.acc.x = -0.5
         if pressed_keys[K_RIGHT]:
             self.acc.x = 0.5  
-        if self.pos.x > 400:
+
+        self.acc.x += self.vel.x * FRIC
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc # 0.5 is from kinematics eqn
+
+        # quick way to allow screen wrapping
+        if self.pos.x > WIDTH:
             self.pos.x = 0
         if self.pos.x < 0:
-            self.pos.x = 400
+            self.pos.x = WIDTH
         
         self.rect.midbottom = self.pos
     
