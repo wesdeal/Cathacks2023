2fa040858c3a1f9d9d031a6d4607aac107428c63
Wes Deal <wesdeall@gmail.com>
2023-04-15 14:18:56 -0400

    change all platform sizes to same size 85x10

--- Platform.py
+++ Platform.py
@@ -5,7 +5,7 @@ from config import *
 class Platform(pg.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.surf = pg.Surface((random.randint(70, 130), 10))
+        self.surf = pg.Surface((85,10))
         self.surf.fill(BLACK)
         self.rect = self.surf.get_rect(topleft=(x, y))
         self.rect.x = x
