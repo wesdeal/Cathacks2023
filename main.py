3bf317feaf8fe9e116709196deaf070c7a6b60c3
David Spivey <davidspivey3.1415@gmail.com>
2023-04-14 21:15:18 -0400

    Create config.py to hold all constants, remove constants and add config import to main.py.

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
 import pygame as pg
 import sys
 from pygame.locals import *
+from config import * # gets all setting constants
 
 pg.init()
-vec = pg.math.Vector2 # used for 2 dimensional movement calculations
-
-# Game performance/display settings
-WIDTH = 500 
-HEIGHT = 500
-FRAMERATE = 60
-
-# Constants for movement to fine tune how it works
-ACC = 0.5
-# Comment this line out for !!FUN!!
-# Also don't make it less than -1 or it also gets !!FUN!!
-FRIC = -0.12
-
-# colors predefined for easy use
-BLACK = pg.Color(0, 0, 0)         
-WHITE = pg.Color(255, 255, 255)   
-GREY = pg.Color(128, 128, 128)   
-RED = pg.Color(255, 0, 0)      
+vec = pg.math.Vector2 # used for 2 dimensional movement calculations     
 
 FPS = pg.time.Clock()
 DISPLAYSURF = pg.display.set_mode((WIDTH, HEIGHT))
