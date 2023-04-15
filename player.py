8419e1da361ca3cc23dc7f4b870cfcec01efda05
David Spivey <davidspivey3.1415@gmail.com>
2023-04-15 11:06:43 -0400

    Player collision detection working during upwards jump, still buggy when going down.

--- player.py
+++ player.py
@@ -8,10 +8,14 @@ vec = pg.math.Vector2
 class Player(pg.sprite.Sprite):
     def __init__(self):
         super().__init__() 
-        self.surf = pg.Surface((30, 30))
+        self.width = 30
+        self.height = 30
+        self.surf = pg.Surface((self.width, self.height))
         self.surf.fill(RED)
         self.rect = self.surf.get_rect()
+        self.jumping = False
 
+        # self.pos corresponds to the center of the Player's rect
         self.pos = vec((50, 0))
         self.vel = vec(0,0)
         self.acc = vec(0,0)
@@ -39,20 +43,40 @@ class Player(pg.sprite.Sprite):
         if self.pos.y < 0:
             self.pos.y = HEIGHT
         
-        self.rect.midbottom = self.pos
+        self.rect.center = self.pos
         
     # used to check for collisions with all the platforms
     def check_platform_collide(self, platforms):
         hits = pg.sprite.spritecollide(self, platforms, False)
-        # Make sure we don't cancel the effect of jump (only stop if there's already velocity)
-        if self.vel.y > 0:
-          if hits: # if you did indeed have a hit
-              self.pos.y = hits[0].rect.top + 1 # Has to be slightly below the first rect you hit
+        if hits:
+
+          # Make sure we don't cancel the effect of jump() (only stop if there's already downward velocity)
+          if self.vel.y > 0:
+              self.pos.y = hits[0].rect.top - self.height / 2
               self.vel.y = 0
+
+          # check to make sure we aren't hitting bottom of a platform
+          if abs(hits[0].rect.bottom - self.rect.top) < 10:
+            self.pos.y = hits[0].rect.bottom + self.height / 2
+            self.vel.y = 0
+
+          # check for player collision on the right of player
+          if abs(hits[0].rect.left - self.rect.right) < 10:
+            self.pos.x = hits[0].rect.left - self.width / 2
+            self.vel.x = 0
+
+          # check for player collision on the left ofnlayer
+          if abs(hits[0].rect.right - self.rect.left) < 10:
+            self.pos.x = hits[0].rect.right + self.width / 2
+            self.vel.x = 0
+            
+
     
     # Requires platform group to be passed in
     def jump(self, platforms):
       hits = pg.sprite.spritecollide(self, platforms, False)
-      if hits:
+      if hits: # only allow jumping if you're actually on a platform
         self.vel.y = -15
-    
+        self.jumping = True
+
+  
