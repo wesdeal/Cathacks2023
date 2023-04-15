5b41c7cdf60f048820459821c1d86e14e7aa8a5c
David Spivey <davidspivey3.1415@gmail.com>
2023-04-14 21:42:21 -0400

    Bugfix on Platform, use correct color format.

--- Platform.py
+++ Platform.py
@@ -1,11 +1,11 @@
 import pygame as pg
-
+from config import *
 
 class Platform(pg.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
         self.surf = pg.Surface((50, 10))
-        self.surf.fill(255,255,255)
+        self.surf.fill(BLACK)
         self.rect = self.surf.get_rect()
         self.rect.x = x
         self.rect.y = y
